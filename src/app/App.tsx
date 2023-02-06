36da6d35647f2eeb5f9dbd369931872d269c34e6
johnk2280 <johnk2280@gmail.com>
2023-02-06 10:25:38 +0500

    [add] FC AppLink and thmes for links

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import { AppRouter } from 'app/providers/router';
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
 
@@ -13,10 +12,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,11 +2,11 @@
 type Mods = Record<string, boolean | string>
 
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
 
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         Object.entries(mods)
             .filter(([key, value]) => Boolean(value))   // key: sting = className, value: boolean
             .map(([key, value]) => key),                // key: sting = className, value: boolean
