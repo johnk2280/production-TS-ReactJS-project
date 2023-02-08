fa8ec1b62f22ab35304f27a68f4681e5bb9bd9e1
johnk2280 <johnk2280@gmail.com>
2023-02-08 09:13:21 +0500

    [add] sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import { AppRouter } from 'app/providers/router';
+import {AppRouter} from 'app/providers/router';
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 const App = () => {
 
@@ -13,7 +13,10 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -9,10 +9,14 @@ const AppRouter = () => {
                 {Object.values(routeConfig).map(({path, element}) => (
                     <Route
                         key={path}
-                        element={element}
+                        element={
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                        }
                         path={path}
                     />
-                    ))}
+                ))}
             </Routes>
         </Suspense>
     );
