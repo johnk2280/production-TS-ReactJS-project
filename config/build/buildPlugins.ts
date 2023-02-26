9770bde7445aba070f0090dbc046e6883aa37625
johnk2280 <johnk2280@gmail.com>
2023-02-26 10:49:16 +0500

    [add] bundle analizer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins ({ mode, paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
@@ -16,6 +17,9 @@ export function buildPlugins ({ mode, paths, isDev }: BuildOptions): webpack.Web
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        })
     ]
 }
