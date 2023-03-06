2e5624c73e75d4a98305e8c8f44a01094b3f84e1
johnk2280 <johnk2280@gmail.com>
2023-03-06 09:07:05 +0500

    [upd] fix unit tests Sidebar - add componentRender

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,15 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import {Sidebar} from "./Sidebar";
-import {renderWithTranslation} from "shared/lib/renderWithTranslation/renderWithTranslation";
+import {renderWithTranslation} from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import {componentRender} from "shared/lib/tests/componentRender/componentRender";
 
 describe('Sidebar', () => {
     test('first', () => {
-        renderWithTranslation(<Sidebar/>)
+        componentRender(<Sidebar/>)
         expect(screen.getByTestId('sidebar'))
     });
     test('second', () => {
-        renderWithTranslation(<Sidebar/>);
+        componentRender(<Sidebar/>);
         const toggleBtn = screen.getByTestId('toggleButton');
         fireEvent.click(toggleBtn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
