d6e73dda9ccee4eb73d539a58635cf305a86af0a
johnk2280 <johnk2280@gmail.com>
2023-03-25 16:02:37 +0500

    [add] test for counter slice

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,19 @@
+import {counterReducer, counterActions} from "./counterSlice";
+import {CombinedState, DeepPartial} from "@reduxjs/toolkit";
+import {StateSchema} from "app/providers/StoreProvider";
+import {CounterSchema} from "entities/Counter";
+
+describe('counterSlice.test', () => {
+    test('decrement', () => {
+        const state: CounterSchema = {value: 10}
+        expect(counterReducer(state, counterActions.decrement())).toEqual({value: 9})
+    })
+})
+
+
+describe('counterSlice.test', () => {
+    test('increment', () => {
+        const state: CounterSchema = {value: 10}
+        expect(counterReducer(state, counterActions.increment())).toEqual({value: 11})
+    })
+})
