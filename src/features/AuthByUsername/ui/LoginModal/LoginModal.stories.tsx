cce292a270b79a731c90ec062588669a8ff305a4
johnk2280 <johnk2280@gmail.com>
2023-05-01 11:51:51 +0500

    [add] stories for LoginForm? LoginModal, Input

---
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+import { LoginModal } from './LoginModal';
+
+export default {
+    title: 'features/LoginModal',
+    component: LoginModal,
+    argTypes: {
+        backgroundColor: { control: 'color' }
+    }
+} as ComponentMeta<typeof LoginModal>;
+
+const Template: ComponentStory<typeof LoginModal> = (args) => <LoginModal { ...args } />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    isOpen: true
+};
