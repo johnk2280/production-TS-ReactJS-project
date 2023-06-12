dc65b86a75b7414836fcf4a351acffe0fa78976d
johnk2280 <johnk2280@gmail.com>
2023-06-12 14:18:59 +0500

    [upd] loginSlice

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -7,12 +7,12 @@ describe('loginSlice.test', () => {
     test('test set username', () => {
         const state: DeepPartial<LoginSchema> = {username: '123'};
         expect(loginReducer(state as LoginSchema, loginActions.setUsername('admin')))
-            .toBe({username: 'admin'});
+            .toEqual({username: 'admin'});
 
     });
     test('test set password', () => {
         const state: DeepPartial<LoginSchema> = {password: '123'};
         expect(loginReducer(state as LoginSchema, loginActions.setPassword('qwerty')))
-            .toBe({username: 'qwerty'});
+            .toEqual({password: 'qwerty'});
     });
 })
