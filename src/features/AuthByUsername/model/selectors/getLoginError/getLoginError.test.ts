f963837e6cb0952e78e4eff55185253a02141483
johnk2280 <johnk2280@gmail.com>
2023-06-12 11:42:53 +0500

    [add] tests for the getLoginError'

---
+++ src/features/AuthByUsername/model/selectors/getLoginError/getLoginError.test.ts
@@ -0,0 +1,22 @@
+import {DeepPartial} from "@reduxjs/toolkit";
+import {StateSchema} from "app/providers/StoreProvider";
+import {getLoginError} from "./getLoginError";
+
+describe('getLoginError.test', () => {
+    test('should return error', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                password: '',
+                username: '',
+                isLoading: false,
+                error: 'error'
+            }
+        }
+        expect(getLoginError(state as StateSchema)).toEqual('error')
+    });
+
+    test('should work wih empty state', () => {
+        const state: DeepPartial<StateSchema> = {}
+        expect(getLoginError(state as StateSchema)).toEqual(undefined)
+    })
+})
