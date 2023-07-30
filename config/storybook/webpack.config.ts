9d09144c6341b82dd634ba89ce9fb67b3285a7ad
johnk2280 <johnk2280@gmail.com>
2023-07-30 09:43:04 +0500

    [upd] webpack.config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,25 +10,25 @@ export default ({config}: { config: webpack.Configuration }): webpack.Configurat
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src')
     }
-    config.resolve?.modules?.push(paths.src);
-    config.resolve?.extensions?.push('.ts', '.tsx');
+    config!.resolve?.modules?.push(paths.src);
+    config!.resolve?.extensions?.push('.ts', '.tsx');
 
     // @ts-ignore
-    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+    config!.module.rules = config.module.rules.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return {...rule, exclude: /\.svg$/i}
         }
         return rule
     })
 
-    config.module?.rules?.push({
+    config!.module?.rules?.push({
         test: /\.svg$/,
         use: ['@svgr/webpack']
     })
-    config.module?.rules?.push(buildCssLoader(true));
+    config!.module?.rules?.push(buildCssLoader(true));
 
     // @ts-ignore
-    config.plugins.push(new DefinePlugin({
+    config!.plugins.push(new DefinePlugin({
         __IS_DEV__: true,
         __API__: JSON.stringify('')
     }))
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,6 +1,5 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import {Sidebar} from "./Sidebar";
-import {renderWithTranslation} from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
 import {componentRender} from "shared/lib/tests/componentRender/componentRender";
 
 describe('Sidebar', () => {
