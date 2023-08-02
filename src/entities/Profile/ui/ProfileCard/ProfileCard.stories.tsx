6375ed1f07557718d934ed3bc01d5a78f31f3ac8
johnk2280 <johnk2280@gmail.com>
2023-08-02 17:27:40 +0500

    [add] storybook for ProfileCard

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { type ComponentMeta, type ComponentStory } from '@storybook/react';
+import { ProfileCard } from './ProfileCard';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
+import Avatar from 'shared/assets/forTests/3b2758ad5492a76b578f7ee072e4e894.jpg';
+
+export default {
+    title: 'entities/ProfileCard',
+    component: ProfileCard,
+    argTypes: {
+        backgroundColor: { control: 'color' }
+    }
+} as ComponentMeta<typeof ProfileCard>;
+
+const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard { ...args } />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    data: {
+        firstname: 'Evgen',
+        lastname: 'Kungurov',
+        age: 42,
+        currency: Currency.RUB,
+        country: Country.Russia,
+        city: 'Chelyabinsk',
+        username: 'admin',
+        avatar: Avatar
+    }
+};
+
+export const withError = Template.bind({});
+withError.args = {
+    error: 'assdfg'
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+    isLoading: true
+};
--- src/shared/ui/Avatar/Avatar.stories.tsx
+++ src/shared/ui/Avatar/Avatar.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { type ComponentStory, type ComponentMeta } from '@storybook/react';
 import { Avatar } from './Avatar';
-import AvatarImg from './3b2758ad5492a76b578f7ee072e4e894.jpg';
+import AvatarImg from '../../assets/forTests/3b2758ad5492a76b578f7ee072e4e894.jpg';
 
 export default {
     title: 'shared/Avatar',
