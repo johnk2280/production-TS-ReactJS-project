282d5933c61c3221f58ba26bf945e6e32d375562
johnk2280 <johnk2280@gmail.com>
2023-08-02 18:11:48 +0500

    [add] tests for getProfileValidateError selector

---
+++ src/entities/Profile/model/selectors/getProfileValidateError/getProfileValidateError.test.ts
@@ -0,0 +1,21 @@
+import {StateSchema} from "app/providers/StoreProvider";
+import {getProfileValidateError} from "./getProfileValidateError";
+import {ValidateProfileError} from "entities/Profile/model/types/profileSchema";
+
+
+describe('getProfileValidateError.test', () => {
+    test('should return error', () => {
+        const validateErrorData: ValidateProfileError[] = [ValidateProfileError.SERVER_ERROR]
+        const state: DeepPartial<StateSchema> = {
+            profile: {
+                validateError: validateErrorData
+            }
+        }
+        expect(getProfileValidateError(state as StateSchema)).toEqual(validateErrorData)
+    });
+
+    test('should work wih empty state', () => {
+        const state: DeepPartial<StateSchema> = {}
+        expect(getProfileValidateError(state as StateSchema)).toEqual(undefined)
+    })
+})
