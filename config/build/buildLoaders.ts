de5f53241e299aa6a9e25fe33d9b14300cd100ea
johnk2280 <johnk2280@gmail.com>
2023-08-04 17:02:35 +0500

    [upd] babelLoader into buildLoaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,8 +1,10 @@
 import type webpack from 'webpack';
 import { type BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
-export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack']
@@ -25,16 +27,7 @@ export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/
     };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    };
+    const babelLoader = buildBabelLoader(options);
 
     return [
         babelLoader,
---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,19 @@
+import { type BuildOptions } from '../types/config';
+
+export function buildBabelLoader (options: BuildOptions): object {
+    const { isDev } = options;
+
+    return {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    isDev && require.resolve('react-refresh/babel')
+                ].filter(Boolean)
+            }
+        }
+    };
+}
