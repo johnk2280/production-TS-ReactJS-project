708f1c727865acb6fd2a8cb773eae8a49f496efb
johnk2280 <johnk2280@gmail.com>
2023-08-05 12:54:49 +0500

    [upd] ArticleDetails - added article skeleton while loading

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -13,6 +13,7 @@ import {
 } from '../../model/selectors/articleDetails';
 import { Text, TextAlign } from 'shared/ui/Text/Text';
 import { useTranslation } from 'react-i18next';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
 
 interface ArticleDetailsProps {
     className?: string;
@@ -32,7 +33,8 @@ export const ArticleDetails: FC<ArticleDetailsProps> = memo((props: ArticleDetai
 
     const dispatch = useAppDispatch();
     const article = useSelector(getArticleDetailsData);
-    const isLoading = useSelector(getArticleDetailsIsLoading);
+    // const isLoading = useSelector(getArticleDetailsIsLoading);
+    const isLoading = true;
     const error = useSelector(getArticleDetailsError);
 
     useEffect(() => {
@@ -43,7 +45,13 @@ export const ArticleDetails: FC<ArticleDetailsProps> = memo((props: ArticleDetai
 
     if (isLoading) {
         content = (
-            <div>Loading...</div>
+            <div>
+                <Skeleton className={ cls.avatar } width={ 200 } height={ 200 } border={ '50%' }/>
+                <Skeleton className={ cls.title } width={ 200 } height={ 32 }/>
+                <Skeleton className={ cls.skeleton } width={ '100%' } height={ 24 }/>
+                <Skeleton className={ cls.skeleton } width={ '100%' } height={ 240 }/>
+                <Skeleton className={ cls.skeleton } width={ '100%' } height={ 240 }/>
+            </div>
         );
     } else if (error) {
         content = (
