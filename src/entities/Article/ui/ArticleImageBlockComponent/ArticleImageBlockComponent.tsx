5c1b9dc75d7ba400dfd008639a00776d64ed4357
johnk2280 <johnk2280@gmail.com>
2023-08-07 18:16:21 +0500

    [add] ArticleImageBlockComponent

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -2,6 +2,7 @@ import { type FC, memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ArticleImageBlockComponent.module.scss';
 import { type ArticleImageBlock } from 'entities/Article/model/types/article';
+import { Text, TextAlign } from 'shared/ui/Text/Text';
 
 interface ArticleImageBlockComponentProps {
     className?: string;
@@ -16,7 +17,8 @@ export const ArticleImageBlockComponent: FC<ArticleImageBlockComponentProps> = m
 
     return (
         <div className={ classNames(cls.ArticleImageBlockComponent, {}, [className]) }>
-
+            <img src={ block.src } alt={ block.title } className={ cls.image }/>
+            { block.title && <Text text={ block.title } align={ TextAlign.CENTER }/> }
         </div>
     );
 });
