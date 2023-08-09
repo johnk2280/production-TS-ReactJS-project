b6040da005a6fb4aa7208433a6e598f2c9a34ddf
johnk2280 <johnk2280@gmail.com>
2023-08-09 18:46:20 +0500

    [add] sendComment, addCommentFormSlice

---
+++ src/features/AddCommentForm/model/services/sendComment/sendComment.ts
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { type ThunkConfig } from 'app/providers/StoreProvider';
+import { type IComment } from 'entities/Comment';
+import { getArticleDetailsData } from 'entities/Article/model/selectors/articleDetails';
+
+export const sendComment = createAsyncThunk<
+IComment,
+void | never,
+ThunkConfig<string>
+>(
+    'addCommentForm/sendComment',
+    async (_, thunkApi) => {
+        const { dispatch, rejectWithValue, extra, getState } = thunkApi;
+        const article = getArticleDetailsData(getState());
+
+        try {
+            const response = await extra.api.post<IComment>('/comments/');
+
+            if (!response.data) {
+                throw new Error();
+            }
+            return response.data;
+        } catch (e) {
+            let message: string = '';
+            if (e instanceof Error) {
+                message = e.message;
+            }
+            return rejectWithValue(message);
+        }
+    }
+);
---
+++ src/features/AddCommentForm/model/slice/addCommentFormSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { type AddCommentFormSchema } from 'features/AddCommentForm';
+
+const initialState: AddCommentFormSchema = {};
+
+export const addCommentFormSlice = createSlice({
+    name: 'addCommentFormSlice',
+    initialState,
+    reducers: {
+        setText: (state, action: PayloadAction<string>) => {
+            state.text = action.payload;
+        }
+    }
+
+});
+
+export const { actions: addCommentFormActions } = addCommentFormSlice;
+export const { reducer: addCommentFormReducer } = addCommentFormSlice;
