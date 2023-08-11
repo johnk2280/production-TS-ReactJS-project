f2974ae3983c18bb0f9619e1b2d39240b3423e1c
johnk2280 <johnk2280@gmail.com>
2023-08-11 18:50:32 +0500

    [add] Card - shared component

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,12 +1,12 @@
 import { type FC, memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ArticleListItem.module.scss';
-import { type Article, type ArticleView } from '../../model/types/article';
+import { type Article, ArticleView } from '../../model/types/article';
 
 interface ArticleListItemProps {
     className?: string;
     article: Article;
-    view?: ArticleView;
+    view: ArticleView;
 }
 
 export const ArticleListItem: FC<ArticleListItemProps> = memo((props: ArticleListItemProps) => {
@@ -15,6 +15,14 @@ export const ArticleListItem: FC<ArticleListItemProps> = memo((props: ArticleLis
         view,
         article
     } = props;
+
+    if (view === ArticleView.BIG) {
+        return (
+            <div className={ classNames(cls.ArticleListItem, {}, [className]) }>
+                { article.title }
+            </div>
+        );
+    }
 
     return (
         <div className={ classNames(cls.ArticleListItem, {}, [className]) }>
---
+++ src/shared/ui/Card/Card.tsx
@@ -0,0 +1,25 @@
+import { type FC, type HTMLAttributes, type ReactNode } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Card.module.scss';
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+    className?: string;
+    children?: ReactNode;
+}
+
+export const Card: FC<CardProps> = (props) => {
+    const {
+        className = '',
+        children,
+        ...otherProps
+    } = props;
+
+    return (
+        <div
+            className={ classNames(cls.Card, {}, [className]) }
+            { ...otherProps }
+        >
+            { children }
+        </div>
+    );
+};
