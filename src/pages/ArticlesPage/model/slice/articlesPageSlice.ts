646c08b8cff287fadc8f7ded439f8c27d9c6ffe2
johnk2280 <johnk2280@gmail.com>
2023-08-14 17:55:33 +0500

    [add] articlesPageSlice

---
+++ src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
@@ -0,0 +1,28 @@
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { type Article, ArticleView } from 'entities/Article';
+import { type StateSchema } from 'app/providers/StoreProvider';
+import { type ArticlesPageSchema } from 'pages/ArticlesPage';
+
+const articlesPageAdapter = createEntityAdapter<Article>({
+    selectId: (article) => article.id
+});
+
+export const getArticles = articlesPageAdapter.getSelectors<StateSchema>(
+    (state) => state.articlesPage ?? articlesPageAdapter.getInitialState()
+);
+
+export const articlesPageSlice = createSlice({
+    name: 'articlesPage',
+    initialState: articlesPageAdapter.getInitialState<ArticlesPageSchema>({
+        ids: [],
+        entities: {},
+        error: undefined,
+        isLoading: false,
+        view: ArticleView.SMALL
+    }),
+    reducers: {
+        setView: (state, action) => {
+            state.view = action.payload;
+        }
+    }
+});
