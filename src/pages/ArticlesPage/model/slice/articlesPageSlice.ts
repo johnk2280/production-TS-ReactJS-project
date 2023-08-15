84a930debc9fe6d94588ca9b16ba9bcb276021ac
johnk2280 <johnk2280@gmail.com>
2023-08-15 16:13:34 +0500

    [upd] articlesPageSlice - added extraReducers

--- src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slice/articlesPageSlice.ts
@@ -2,6 +2,7 @@ import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 import { type Article, ArticleView } from 'entities/Article';
 import { type StateSchema } from 'app/providers/StoreProvider';
 import { type ArticlesPageSchema } from '../types/articlesPageSchema';
+import { fetchArticles } from 'pages/ArticlesPage/model/services/fetchArticles/fetchArticles';
 
 const articlesPageAdapter = createEntityAdapter<Article>({
     selectId: (article) => article.id
@@ -24,5 +25,21 @@ export const articlesPageSlice = createSlice({
         setView: (state, action) => {
             state.view = action.payload;
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchArticles.pending, (state) => {
+                state.error = '';
+                state.isLoading = true;
+            })
+            .addCase(fetchArticles.fulfilled, (state, action) => {
+                state.error = '';
+                state.isLoading = false;
+                articlesPageAdapter.setAll(state, action.payload);
+            })
+            .addCase(fetchArticles.rejected, (state, action) => {
+                state.error = action.payload ?? 'error';
+                state.isLoading = false;
+            });
     }
 });
