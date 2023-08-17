0981e5ed48ef0928d96d17cb1f3f06eac359b86f
johnk2280 <johnk2280@gmail.com>
2023-08-17 19:50:20 +0500

    [upd] getCommentForm - fixed input

--- src/features/AddCommentForm/model/selectors/getCommentForm.ts
+++ src/features/AddCommentForm/model/selectors/getCommentForm.ts
@@ -1,5 +1,5 @@
 import { type StateSchema } from 'app/providers/StoreProvider';
 
-export const getCommentFormText = (state: StateSchema): string | undefined => state.addCommentForm?.text;
+export const getCommentFormText = (state: StateSchema): string => state.addCommentForm?.text ?? '';
 export const getCommentFormError = (state: StateSchema): string | undefined => state.addCommentForm?.error;
 export const getCommentFormIsLoading = (state: StateSchema): boolean | undefined => state.addCommentForm?.isLoading;
