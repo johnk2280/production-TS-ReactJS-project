90303c3043dda4c6c7923f78e39ca1103b0b5425
johnk2280 <johnk2280@gmail.com>
2023-08-20 11:20:19 +0500

    [add] Tabs component

---
+++ src/shared/ui/Tabs/Tabs.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+import { Tabs } from './Tabs';
+
+export default {
+    title: 'shared/Tabs',
+    component: Tabs,
+    argTypes: {
+        backgroundColor: { control: 'color' }
+    }
+} as ComponentMeta<typeof Tabs>;
+
+const Template: ComponentStory<typeof Tabs> = (args) => <Tabs { ...args } />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
---
+++ src/shared/ui/Tabs/Tabs.tsx
@@ -0,0 +1,51 @@
+import { type FC, memo, type ReactNode, useCallback } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Tabs.module.scss';
+import { Card, CardTheme } from 'shared/ui/Card/Card';
+
+interface TabItem {
+    value: string;
+    content: ReactNode;
+}
+
+interface TabsProps {
+    className?: string;
+    tabs: TabItem[];
+    value: string;
+    onTabClick: (tab: TabItem) => void;
+}
+
+export const Tabs: FC<TabsProps> = memo((props: TabsProps) => {
+    const {
+        className = '',
+        tabs,
+        onTabClick,
+        value
+    } = props;
+
+    // Используется замыкание
+    const clickHandle = useCallback((newTab: TabItem) => {
+        return () => {
+            onTabClick(newTab);
+        };
+    }, [onTabClick]);
+
+    return (
+        <div className={ classNames(cls.Tabs, {}, [className]) }>
+            {
+                tabs.map(tab => (
+                    <Card
+                        theme={ tab.value === value ? CardTheme.NORMAL : CardTheme.OUTLINED }
+                        className={ cls.tab }
+                        key={ tab.value }
+                        onClick={ () => clickHandle(tab) }
+                    >
+                        { tab.content }
+                    </Card>
+                ))
+            }
+        </div>
+    );
+});
+
+Tabs.displayName = 'Tabs';
