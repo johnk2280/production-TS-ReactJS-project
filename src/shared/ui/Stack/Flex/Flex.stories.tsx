bbc61f91965565243cdd3826e6ee5f1cfaff7f04
johnk2280 <johnk2280@gmail.com>
2023-08-29 19:31:59 +0500

    [upd] Flex, Flex.stories - added gap"

--- src/shared/ui/Stack/Flex/Flex.stories.tsx
+++ src/shared/ui/Stack/Flex/Flex.stories.tsx
@@ -44,3 +44,68 @@ Column.args = {
         </>
     )
 };
+
+export const Gap4 = Template.bind({});
+Gap4.args = {
+    gap: '4',
+    children: (
+        <>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>one</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>two</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>three</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>four</div>
+        </>
+    )
+};
+export const Gap8 = Template.bind({});
+Gap8.args = {
+    gap: '8',
+    children: (
+        <>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>one</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>two</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>three</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>four</div>
+        </>
+    )
+};
+export const Gap16 = Template.bind({});
+Gap16.args = {
+    gap: '16',
+    children: (
+        <>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>one</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>two</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>three</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>four</div>
+        </>
+    )
+};
+export const Gap32 = Template.bind({});
+Gap32.args = {
+    gap: '32',
+    children: (
+        <>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>one</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>two</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>three</div>
+            { /* eslint-disable-next-line i18next/no-literal-string */ }
+            <div>four</div>
+        </>
+    )
+};
--- src/shared/ui/Stack/Flex/Flex.tsx
+++ src/shared/ui/Stack/Flex/Flex.tsx
@@ -5,6 +5,7 @@ import { type FC, type ReactNode } from 'react';
 export type FlexJustify = 'start' | 'center' | 'end' | 'between';
 export type FlexAlign = 'start' | 'center' | 'end';
 export type FlexDirection = 'row' | 'column';
+export type FlexGap = '4' | '8' | '16' | '32';
 
 interface FlexProps {
     className?: string;
@@ -12,6 +13,7 @@ interface FlexProps {
     justify?: FlexJustify;
     align?: FlexAlign;
     direction: FlexDirection;
+    gap?: FlexGap;
 }
 
 const justifyClasses: Record<FlexJustify, string> = {
@@ -31,20 +33,29 @@ const directionClasses: Record<FlexDirection, string> = {
     column: cls.directionColumn
 };
 
+const gapClasses: Record<FlexGap, string> = {
+    4: cls.gap4,
+    8: cls.gap8,
+    16: cls.gap16,
+    32: cls.gap32
+};
+
 export const Flex: FC<FlexProps> = (props: FlexProps) => {
     const {
         className = '',
         children,
         justify = 'start',
         align = 'center',
-        direction = 'row'
+        direction = 'row',
+        gap = '8'
     } = props;
 
     const classes: string[] = [
         className,
         justifyClasses[justify],
         alignClasses[align],
-        directionClasses[direction]
+        directionClasses[direction],
+        gapClasses[gap]
     ];
 
     return (
