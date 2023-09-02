38e4e91bf6d4cfde4706c3a567772721c15d2483
johnk2280 <johnk2280@gmail.com>
2023-09-02 17:10:43 +0500

    [upd] Dropdown - added styles

--- src/shared/ui/Dropdown/ui/Dropdown.stories.tsx
+++ src/shared/ui/Dropdown/ui/Dropdown.stories.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {type ComponentStory, type ComponentMeta} from '@storybook/react';
-import {Dropdown} from './Dropdown';
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+import { Dropdown } from './Dropdown';
 
 export default {
     title: 'shared/Dropdown',
     component: Dropdown,
     argTypes: {
-        backgroundColor: {control: 'color'}
+        backgroundColor: { control: 'color' }
     }
 } as ComponentMeta<typeof Dropdown>;
 
-const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown {...args} />;
+const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown { ...args } />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
--- src/shared/ui/Dropdown/ui/Dropdown.tsx
+++ src/shared/ui/Dropdown/ui/Dropdown.tsx
@@ -1,24 +1,42 @@
-import { type FC, memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Dropdown.module.scss';
 import { Menu } from '@headlessui/react';
+import { VStack } from '../../../ui/Stack';
+import { Fragment } from 'react';
 
-export function Dropdown (): JSX.Element {
+export interface DropdownProps {
+    className?: string;
+}
+
+export function Dropdown (props: DropdownProps): JSX.Element {
+    const {
+        className
+    } = props;
     return (
-        <Menu>
-            <Menu.Button>More</Menu.Button>
-            <Menu.Items>
-                <Menu.Item>
-                    { ({ active }) => (
-                        <a
-                            className={ `${active && 'bg-blue-500'}` }
-                            href="/account-settings"
-                        >
-                            Account settings
-                        </a>
-                    ) }
-                </Menu.Item>
+        <Menu
+            as={ 'div' }
+            className={ classNames(cls.Dropdown, {}, [className ?? '']) }
+        >
+            <Menu.Button className={ cls.btn }>
+                More
+            </Menu.Button>
+            <Menu.Items className={ cls.menu }>
+                <VStack gap={ '8' }>
+                    <Menu.Item as={ Fragment }>
+                        { ({ active, disabled }) => (
+                            <li
+                                className={
+                                    classNames(cls.item, { [cls.active]: active, [cls.disabled]: disabled })
+                                }
+                            >
+                                Account settings
+                            </li>
+                        ) }
+                    </Menu.Item>
 
+                </VStack>
+
+            </Menu.Items>
         </Menu>
     );
 }
