503fedd0d96b77a5abc1e773d18d216c3328eb8e
johnk2280 <johnk2280@gmail.com>
2023-09-04 20:16:48 +0500

    [add] recommendationsApi

--- src/features/ArticleRecommendationsList/ui/ArticleRecommendationsList.tsx
+++ src/features/ArticleRecommendationsList/ui/ArticleRecommendationsList.tsx
@@ -4,16 +4,38 @@ import { VStack } from 'shared/ui/Stack';
 import { Text, TextSize } from 'shared/ui/Text/Text';
 import { ArticleList } from 'entities/Article';
 import { useTranslation } from 'react-i18next';
+import { rtkApi } from 'shared/api/rtkApi';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 
 interface ArticleRecommendationsListProps {
     className?: string;
 }
+
+const recommendationsApi = rtkApi.injectEndpoints({
+    endpoints: (build) => ({
+        getArticleRecommendations: build.query({
+            query: (limit) => ({
+                url: RoutePath.articles,
+                params: {
+                    _limit: limit
+                }
+            })
+        })
+    })
+});
+
+export const { useGetArticleRecommendationsQuery } = recommendationsApi;
 
 export const ArticleRecommendationsList: FC<ArticleRecommendationsListProps> = memo((props: ArticleRecommendationsListProps) => {
     const {
         className = ''
     } = props;
     const { t } = useTranslation('article-details');
+    const {
+        isLoading,
+        data: articles,
+        error
+    } = useGetArticleRecommendationsQuery(3);
 
     return (
         <VStack
@@ -25,7 +47,7 @@ export const ArticleRecommendationsList: FC<ArticleRecommendationsListProps> = m
                 size={ TextSize.L }
             />
             <ArticleList
-                articleList={ [] }
+                articleList={ articles }
                 target={ '_blank' }
             />
 
