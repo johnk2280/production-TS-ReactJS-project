db8a6775ed7a80d6e6b5436cd6c276580e19c1f0
johnk2280 <johnk2280@gmail.com>
2023-09-09 12:22:46 +0500

    [upd] EditableProfileCard - delete styles module

--- src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
+++ src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
@@ -1,6 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import cls from './EditableProfileCard.module.scss';
 import { memo, useCallback } from 'react';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useSelector } from 'react-redux';
@@ -86,7 +85,7 @@ export const EditableProfileCard = memo((props: EditableProfileCardProps) => {
         <DynamicModuleLoader reducers={ reducers } removeAfterUnmount>
             <VStack
                 max={ true }
-                className={ classNames(cls.EditableProfileCard, {}, [className]) }
+                className={ classNames('', {}, [className]) }
             >
                 <EditableProfileCardHeader/>
                 {
