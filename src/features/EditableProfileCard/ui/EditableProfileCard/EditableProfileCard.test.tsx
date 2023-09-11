88c1b64161aee887ebc257b10cd57004121ec2a3
johnk2280 <johnk2280@gmail.com>
2023-09-11 19:37:37 +0500

    [upd] added second test for EditableProfileCard

--- src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
+++ src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
@@ -19,28 +19,48 @@ const profile: ProfileType = {
     avatar: "https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg"
 }
 
+const options = {
+    initialState: {
+        profile: {
+            readonly: true,
+            error: '',
+            isLoading: false,
+            data: profile,
+            form: profile,
+            validateError: []
+        },
+        user: {
+            authData: {id: '1'}
+        }
+    },
+    asyncReducers: {
+        profile: profileReducer,
+    },
+}
 
 describe('Sidebar', () => {
     test('Переключение режима reedonly на редактирование', async () => {
-        componentRender(<EditableProfileCard id={'1'}/>, {
-            initialState: {
-                profile: {
-                    readonly: true,
-                    error: '',
-                    isLoading: false,
-                    data: profile,
-                    form: profile,
-                    validateError: []
-                },
-                user: {
-                    authData: {id: '1'}
-                }
-            },
-            asyncReducers: {
-                profile: profileReducer,
-            },
-        })
+        componentRender(<EditableProfileCard id={'1'}/>, options)
         await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
         expect(screen.getByTestId('EditableProfileCardHeader.CancelButton')).toBeInTheDocument();
+    });
+
+    test('При отмене значения обнуляются', async () => {
+        componentRender(<EditableProfileCard id={'1'}/>, options)
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstname'));
+        await userEvent.clear(screen.getByTestId('ProfileCard.lastname'));
+
+        await userEvent.type(screen.getByTestId('ProfileCard.firstname'), 'qwerty');
+        await userEvent.type(screen.getByTestId('ProfileCard.lastname'), 'qwerty');
+
+        expect(screen.getByTestId('ProfileCard.firstname')).toHaveValue('qwerty');
+        expect(screen.getByTestId('ProfileCard.lastname')).toHaveValue('qwerty');
+
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.CancelButton'));
+
+        expect(screen.getByTestId('ProfileCard.firstname')).toHaveValue('Super Evgen');
+        expect(screen.getByTestId('ProfileCard.lastname')).toHaveValue('Kungurov');
     });
 })
