dde4a61845019474f3546bf4f893797d8b0f36e9
johnk2280 <johnk2280@gmail.com>
2023-09-16 17:00:23 +0500

    [upd] storybook config

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -11,4 +11,4 @@ module.exports = {
     core: {
         builder: '@storybook/builder-webpack5'
     }
-}
+};
--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -1,12 +1,11 @@
 import { type Story } from '@storybook/react';
 import 'app/styles/index.scss';
-import { type ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-export const RouterDecorator = (ComponentStory: Story): ReactNode => {
+export const RouterDecorator = (StoryComponent: Story): JSX.Element => {
     return (
         <BrowserRouter>
-            <ComponentStory/>
+            <StoryComponent/>
         </BrowserRouter>
     );
 };
