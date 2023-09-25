c265084f561f574a9afba8f6719eed42a998b9d5
johnk2280 <johnk2280@gmail.com>
2023-09-25 20:18:19 +0500

    [add] useModal hook

---
+++ src/shared/lib/hooks/useModal/useModal.ts
@@ -0,0 +1,69 @@
+import { useState, useRef, useEffect, useCallback } from 'react';
+
+export interface UseModalProps {
+    isOpen?: boolean;
+    onClose?: () => void;
+    animationDelay: number;
+}
+
+export interface UseModalReturn {
+    isClosing: boolean;
+    isMounted: boolean;
+    close: () => void;
+}
+
+export const useModal = (props: UseModalProps): UseModalReturn => {
+    const {
+        animationDelay,
+        isOpen = false,
+        onClose
+    } = props;
+
+    const [isClosing, setIsClosing] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+    const timeRef = useRef<ReturnType<typeof setTimeout>>(); // ссылка используется для реализации анимации закрытия, в нее передается таймер
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+        }
+    }, [isOpen]);
+
+    const close = useCallback((): void => {
+        // Функция закрытия модального окна
+        // Закрытие срабатывает только если был передан коллбэк onClose()
+        if (onClose != null) {
+            setIsClosing(true);
+            timeRef.current = setTimeout(() => {
+                // Данный таймер реализован для анимации плавного закрытия контентной части модально окна
+                onClose();
+                setIsClosing(false);
+            }, animationDelay);
+        }
+    }, [animationDelay, onClose]);
+
+    const onKeyDown = useCallback((e: KeyboardEvent): void => {
+        if (e.key === 'Escape') {
+            console.log(e.key);
+            close();
+        }
+    }, [close]);
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+        return () => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            clearTimeout(timeRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
+    return {
+        isClosing,
+        isMounted,
+        close
+    };
+};
