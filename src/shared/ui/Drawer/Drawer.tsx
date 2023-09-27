d14ce3cc0707a54c5ffa078429b780a8666ab917
johnk2280 <johnk2280@gmail.com>
2023-09-27 20:21:54 +0500

    [upd] Drawer - added @use-gesture/react, @react-spring/web

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,8 +1,8 @@
+import { a, useSpring, config } from '@react-spring/web';
+import { useDrag } from '@use-gesture/react';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { type FC, memo, type ReactNode } from 'react';
-import { useAnimationLibs } from 'shared/lib/components/AnimationProvider';
-import { useModal } from 'shared/lib/hooks/useModal/useModal';
-import { classNames, type Mods } from '../../lib/classNames/classNames';
+import { type FC, memo, type ReactNode, useCallback, useEffect } from 'react';
+import { classNames } from '../../lib/classNames/classNames';
 import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal';
 import cls from './Drawer.module.scss';
@@ -15,7 +15,9 @@ interface DrawerProps {
     lazy?: boolean;
 }
 
-export const DrawerContent: FC<DrawerProps> = memo((props: DrawerProps) => {
+const height = window.innerHeight - 100;
+
+export const Drawer: FC<DrawerProps> = memo((props: DrawerProps) => {
     const {
         className = '',
         children,
@@ -23,47 +25,86 @@ export const DrawerContent: FC<DrawerProps> = memo((props: DrawerProps) => {
         isOpen,
         lazy
     } = props;
+
+    const [{ y }, api] = useSpring(() => ({ y: height }));
     const { theme } = useTheme();
 
-    const { isMounted, isClosing, close } = useModal({
-        isOpen,
-        onClose,
-        animationDelay: 300
-    });
+    const openDrawer = useCallback(() => {
+        api.start({ y: 0, immediate: false });
+    }, [api]);
 
-    const mods: Mods = {
-        [cls.opened]: isOpen,
-        [cls.isClosing]: isClosing
+    useEffect(() => {
+        if (isOpen) {
+            openDrawer();
+        }
+    }, [isOpen, openDrawer]);
+
+    const closeDrawer = (velocity = 0): void => {
+        api.start({
+            y: height,
+            immediate: false,
+            config: { ...config.stiff, velocity },
+            onResolve: onClose
+        });
     };
 
-    if (lazy && !isMounted) {
+    const bind = useDrag(
+        ({
+            last,
+            velocity: [, vy],
+            direction: [, dy],
+            offset: [, oy],
+            cancel
+        }) => {
+            if (oy < -70) cancel();
+
+            if (last) {
+                oy > height * 0.5 || (vy > 0.5 && dy > 0) ? closeDrawer() : openDrawer();
+            } else {
+                api.start({ y: oy, immediate: true });
+            }
+        },
+        {
+            from: () => [0, y.get()],
+            filterTaps: true,
+            bounds: { top: 0 },
+            rubberband: true
+        }
+    );
+
+    if (!isOpen) {
         return null;
     }
+
+    const display = y.to((py) => (py < height ? 'block' : 'none'));
 
     return (
         <Portal>
-            <div className={ classNames(cls.Drawer, mods, [className, theme ?? '', 'app_drawer']) }>
-                <Overlay onClick={ close }/>
-                <div className={ cls.content }>
+            <div className={ classNames(cls.Drawer, {}, [className, theme ?? '', 'app_drawer']) }>
+                <Overlay onClick={ closeDrawer }/>
+                <a.div
+                    className={ cls.sheet }
+                    style={ { display, bottom: `calc(-100vh + ${height - 100}px)`, y } }
+                    { ...bind() }
+                >
                     { children }
-                </div>
+                </a.div>
             </div>
         </Portal>
-
     );
 });
 
-export const Drawer = memo((props: DrawerProps) => {
-    const { isLoaded } = useAnimationLibs();
-
-    if (!isLoaded) {
-        return null;
-    }
-
-    return (
-        <DrawerContent { ...props }>{ props.children }</DrawerContent>
-    );
-});
+// export const Drawer = memo((props: DrawerProps) => {
+//     const { isLoaded } = useAnimationLibs();
+//
+//     if (!isLoaded) {
+//         return null;
+//     }
+//
+//     return (
+//         <DrawerContent { ...props }>{ props.children }</DrawerContent>
+//     );
+// });
 
 Drawer.displayName = 'Drawer';
-DrawerContent.displayName = 'DrawerContent';
+// DrawerContent.displayName = 'DrawerContent';
