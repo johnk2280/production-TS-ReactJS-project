ccc8078131a580921571ce6819bf943e208509f6
johnk2280 <johnk2280@gmail.com>
2023-09-30 14:01:10 +0500

    [upd] StarRating - added stars hovering handling

--- src/shared/ui/StarRating/StarRating.tsx
+++ src/shared/ui/StarRating/StarRating.tsx
@@ -1,19 +1,40 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Icon } from '@/shared/ui/Icon/Icon';
-import { type FC, memo } from 'react';
+import { type FC, memo, useState, useCallback } from 'react';
 import StarIcon from '../../assets/icons/star-20-20.svg';
 import cls from './StarRating.module.scss';
 
 interface StarRatingProps {
     className?: string;
+    onSelect?: (starsCount: number) => void;
+    size?: number;
+    selectedStars?: number;
 }
 
 const stars = [1, 2, 3, 4, 5];
 
 export const StarRating: FC<StarRatingProps> = memo((props: StarRatingProps) => {
     const {
-        className = ''
+        className = '',
+        size = 30,
+        selectedStars = 0,
+        onSelect
     } = props;
+
+    const [currentStarsCount, setCurrentStarsCount] = useState(0);
+    const [isSelected, setIsSelected] = useState(Boolean(selectedStars));
+
+    const onHover = useCallback((starNumber: number) => () => {
+        if (!isSelected) {
+            setCurrentStarsCount(starNumber);
+        }
+    }, [isSelected]);
+
+    const onLeave = useCallback(() => {
+        if (!isSelected) {
+            setCurrentStarsCount(0);
+        }
+    }, [isSelected]);
 
     return (
         <div className={ classNames(cls.StarRating, {}, [className]) }>
@@ -22,7 +43,11 @@ export const StarRating: FC<StarRatingProps> = memo((props: StarRatingProps) => 
                     <Icon
                         Svg={ StarIcon }
                         key={ starNumber }
-
+                        className={ classNames(cls.icon, { [cls.hovered]: currentStarsCount >= starNumber }) }
+                        height={ size }
+                        width={ size }
+                        onMouseEnter={ onHover(starNumber) }
+                        onMouseLeave={ onLeave }
                     />
                 ))
             }
