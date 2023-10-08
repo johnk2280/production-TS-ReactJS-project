7becda5b92217821d991b920a082dc3edc11c9ef
johnk2280 <johnk2280@gmail.com>
2023-10-08 10:59:23 +0500

    [add] articleRatingApi

---
+++ src/features/ArticleRating/api/articleRatingApi.ts
@@ -0,0 +1,18 @@
+import { type Rating } from '@/entities/Rating';
+import { rtkApi } from '@/shared/api/rtkApi';
+
+const articleRatingApi = rtkApi.injectEndpoints({
+    endpoints: (build) => ({
+        getArticleRating: build.query<Rating, { userId: string; articleId: string }>({
+            query: ({ userId, articleId }) => ({
+                url: '/article-ratings',
+                params: {
+                    userId,
+                    articleId
+                }
+            })
+        })
+    })
+});
+
+export const { useGetArticleRatingQuery } = articleRatingApi;
